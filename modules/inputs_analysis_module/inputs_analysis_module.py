5cc2b5545ec410e1ad1d2e372d34ae45d3d8e4de
Ahmed El-Shamy <ahmed.osama.elshamy@gmail.com>
2026-09-15 21:29:50 +0300

    feat: add constructor to InputsAnalysisModule class

--- modules/inputs_analysis_module/inputs_analysis_module.py
+++ modules/inputs_analysis_module/inputs_analysis_module.py
@@ -4,6 +4,17 @@ from user_description_model import UserDescriptionModel
 from images_model import ImagesModel
 
 class InputsAnalysisModule:
+    def __init__(
+            self, 
+            invoice_model: InvoiceModel,
+            police_model: PoliceReportModel,
+            user_description_model: UserDescriptionModel,
+            images_model: ImagesModel
+    ):
+        self.invoice_model = invoice_model
+        self.police_model = police_model
+        self.user_description_model = user_description_model
+        self.images_model = images_model
 
     def extract_information(self, images, description, invoice, police_report) -> dict:
         pass
