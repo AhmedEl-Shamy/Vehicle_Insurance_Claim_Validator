1b47e63bb623f1c0c5e898dc64ae2b25b33c3a83
Mariam Anwer Mohammed Youssef <162914295+MariamAnwer820@users.noreply.github.com>
2026-09-17 01:19:02 +0300

    vision classes

---
+++ modules/inputs_analysis_module/__init__.py
@@ -0,0 +1,7 @@
+from .validation_image import ValidationImage
+from .image_detection_segmentation import ImageDetectionSegmentation
+
+__all__ = [
+    "ValidationImage",
+    "ImageDetectionSegmentation",
+]
