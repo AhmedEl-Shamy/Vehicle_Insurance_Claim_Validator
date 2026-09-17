107b46d538659c20df7ce1c1234bed1b52ce8f8d
Ahmed El-Shamy <ahmed.osama.elshamy@gmail.com>
2026-09-17 13:12:54 +0300

    remove __init__.py from inputs_analysis_module

--- modules/inputs_analysis_module/__init__.py
+++
@@ -1,7 +0,0 @@
-from .validation_image import ValidationImage
-from .image_detection_segmentation import ImageDetectionSegmentation
-
-__all__ = [
-    "ValidationImage",
-    "ImageDetectionSegmentation",
-]
